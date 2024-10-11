4fbea90e2a4025a04ee4efe79080ecfadf81622e
aegarmus <aegarmus@gmail.com>
2024-10-11 20:20:01 -0300

    feat: Add two diferent ways to organize code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 import { Digimon } from "./src/model/Digimon.js"
-import { fetchAllDigimon } from "./src/services/fetchDigimon.js"
-
+import { Components } from "./src/components/Components.js"
+import { Render } from "./src/render/Render.js"
 
 
 const digimonContainer = document.querySelector('#all-digimon')
@@ -10,81 +10,7 @@ const buttonSearchDigimon = document.querySelector('#search-digimon')
 const oneDigimonContainer = document.querySelector('#one-digimon')
 
 
-const createOneCardDigimon = (digimon) => {
-    return `
-    <div class="card">
-        <div class="card__header">
-            <img src="${digimon.image}" alt="${digimon.name}" />
-            <h2>${digimon.name}</h2>
-        </div>
-        <div class="card__body">
-              <ul>
-                <li>Level: ${digimon.level[0]}</li>
-                <div>
-                    <p>attributes</p>
-
-                    ${digimon.attributes
-                        .map((attribute) => `<li>${attribute}</li>`)
-                        .join("")}
-                </div>
-                <div>
-                    <p>Types</p>
-
-                    ${digimon.types.map((type) => `<li>${type}</li>`).join("")}
-                </div>
-                <div>
-                    <p>Fields</p>
-
-                    ${digimon.fields
-                        .map(
-                          (field) =>
-                            `<li>
-                              <div>
-                                  <p>${field.field}</p>
-                                  <img src="${field.image}" alt="imagen de ${field.field}" />
-                              </div>
-                          </li>`
-                        )
-                        .join("")}
-                </div>
-            </ul>
-            <p>${digimon.description.description}</p>
-            <p>Año de lanzamiento: ${digimon.release}</p>
-        </div>
-    </div>
-    `;
-}
-
-
-const createCardDigimon = (digimon) => {
-    return `
-    <div class="card">
-        <div class="card__header">
-            <img src="${digimon.image}" alt="foto de ${digimon.name}" />
-        </div>
-        <div class="card__body">
-            <h2>${digimon.name}</h2>
-            <p>N° ${digimon.id}</p>
-        </div>
-    </div
-    `
-}
-
-const renderCardDigimon = (contenedor, cardDigimon) => {
-    contenedor.innerHTML += cardDigimon
-}
-
-const renderAllDigimon = async() => {
-    const allDigimon = await fetchAllDigimon()
-    const digimonList = await allDigimon.content
-
-    digimonList.forEach(digimon => {
-        const cardDigimon = createCardDigimon(digimon)
-        renderCardDigimon(digimonContainer, cardDigimon)
-    })
-}
-
-renderAllDigimon()
+Render.allDigimon(digimonContainer);
 
 buttonSearchDigimon.addEventListener('click', async() => {
     try {
@@ -93,9 +19,9 @@ buttonSearchDigimon.addEventListener('click', async() => {
         if(!digimonSearched) throw new Error('Debes ingresar un ID o nombre valido')
         
         const digimonData = await Digimon.createDigimon(digimonSearched)
-        const cardDigimon = createOneCardDigimon(digimonData)
+        const cardDigimon = Components.oneCardDigimon(digimonData)
     
-        renderCardDigimon(oneDigimonContainer, cardDigimon)
+        Render.oneCardDigimon(oneDigimonContainer, cardDigimon)
         
     } catch (error) {
         console.error('No pudimos cargar el digimon en la vista', error)
